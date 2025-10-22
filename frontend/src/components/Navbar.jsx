7ce74fd6382cd24af510de30094f58da786a69ff
Luka Jekic <lukajekic913@gmail.com>
2025-10-22 18:13:37 +0200

    Improvements

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
     <header  className="bg-base-300 border-b border-base-content/10">
         <div className="mx-auto max-w-6xl p-4">
 <div className="flex items-center justify-between">
-    <h1 className="text-3xl font-bold text-primary font tracking-tighter">ThinkBoard</h1>
+    <h1 className="text-3xl font-bold text-primary font tracking-tighter">MERN_Notes</h1>
     <div className="flex items-center gap-4">
         <Link to="/create" className="btn btn-primary" >
         <PlusIcon className="size-5"/>
