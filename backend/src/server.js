63a016e0342ab368c55879e5f56bf0f2db45dee6
Luka Jekic <lukajekic913@gmail.com>
2025-10-22 17:09:49 +0200

    Preparing for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,11 +5,13 @@ import dotenv from 'dotenv'
 import rateLimiter from './middleware/rateLimiter.js'
 dotenv.config()
 import cors from 'cors'
+import path from 'path'
 
 
-
-
+if (process.env.NODE_ENV !== "production") {
 app.use(cors())
+
+}
 
 app.use(express.json()) //parse json body;
 
@@ -18,6 +20,16 @@ import notesRouters from './routes/notesRoutes.js'
 app.use(rateLimiter)
 
 app.use('/api/notes', notesRouters)
+const __dirname = path.resolve()
+
+app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+
+if(process.env.NODE_ENV === "production") {
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, "../frontend/", "dist", "index.html"))
+})
+}
 
 connectDB().then(()=>{
     const PORT  = process.env.PORT || 5001;
