66f3921703573af5be1595e484bae8127a4eb5c4
Luka Jekic <lukajekic913@gmail.com>
2025-10-22 17:27:59 +0200

    API rate limit update

--- backend/src/config/upstash.js
+++ backend/src/config/upstash.js
@@ -6,7 +6,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 const ratelimit = new Ratelimit({
  redis: Redis.fromEnv(),
- limiter: Ratelimit.slidingWindow("10", "20s")
+ limiter: Ratelimit.slidingWindow(100, "20s")
 
 })
 
